357b84be6be14e0287d79f8c1c3ff669b22aaf31
Zakharchenko Tanya <zubarevatanya@gmail.com>
2022-02-04 13:18:07 +0400

    feat: complete task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const result = []
+  for(let i=0; i<str.length; i++){
+    const s = str[i]
+    if (result.length === 0 || result[result.length - 1] !== s) {
+      const closeBracket = bracketsConfig.find((arr) => {
+        return arr[0] === s
+      })
+      if (closeBracket == undefined) {
+        return false
+      }
+      result.push(closeBracket[1])
+    } else {
+      result.pop()
+    }
+  }
+  return result.length === 0
 }
